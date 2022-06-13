82ab42a7d84b3887ad6752c9c7b987d55ac4dcc8
Colin Goudie <colin.goudie@tributech.com.au>
2022-06-13 13:05:38 +1000

    Handled optional flag instead

--- lib/acts_as_tenant.rb
+++ lib/acts_as_tenant.rb
@@ -119,7 +119,7 @@ module ActsAsTenant
 
   def self.should_require_tenant?
     if configuration.require_tenant.respond_to?(:call)
-      !!configuration.require_tenant.call(@@tenant_klass)
+      !!configuration.require_tenant.call
     else
       !!configuration.require_tenant
     end
